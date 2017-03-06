5b807e87e3dca74c0b77c46764b5bfe4c0dc6b66
hexy <hexy@cdfortis.com>
2017-03-06 14:20:02 +0800

    增加跳转逻辑

--- Simple/Classes/SPUIManager.m
+++ Simple/Classes/SPUIManager.m
@@ -21,9 +21,89 @@
     return _sharedInstance;
 }
 
--(void)pushViewController:(UIViewController *)controller fromController:(UIViewController *)fromController{
+-(void)pushViewController:(UIViewController *)controller{
+    if (!controller || ![controller isKindOfClass:[UIViewController class]]) {
+        return;
+    }
     
+    if (self.topViewController.presentedViewController) {
+        [self.topViewController.presentedViewController dismissViewControllerAnimated:YES completion:nil];
+    }
+    
+    if (self.topViewController.navigationController) {
+        [self.topViewController.navigationController pushViewController:controller animated:YES];
+    }
 }
--(void)presentViewController:(UIViewController *)controller fromController:(UIViewController *)fromController{
+
+-(void)presentViewController:(UIViewController *)controller{
+    if (!controller || ![controller isKindOfClass:[UIViewController class]]) {
+        return;
+    }
+    
+    if (self.topViewController.presentedViewController) {
+        [self.topViewController.presentedViewController dismissViewControllerAnimated:YES completion:nil];
+    }
+    
+    [self.topViewController presentViewController:controller animated:YES completion:nil];
 }
+
+-(UIViewController *)topViewController{
+    if (!_topViewController) {
+        _topViewController = [self topViewController];//如果viewcontroller为空的情况，才会走这里
+    }
+    
+    return _topViewController;
+}
+
+//仅仅满足主流TABBAR 和 NAVGATION的框架
+- (UIViewController *)topmostViewController
+{
+    //rootViewController需要是TabBarController,排除正在显示FirstPage的情况
+    UIViewController *rootViewContoller = [UIApplication sharedApplication].delegate.window.rootViewController;
+    if (!rootViewContoller) {
+        return nil;
+    }
+    //当前显示哪个tab页
+    UIViewController *rootNavController = nil;
+    if([rootViewContoller isKindOfClass:[UITabBarController class]]){
+        rootNavController = [(UITabBarController*)rootViewContoller selectedViewController];
+    }else if([rootViewContoller isKindOfClass:[UINavigationController class]]){
+        rootNavController = (UINavigationController *)rootViewContoller;
+    } else {
+        rootNavController = rootViewContoller;
+    }
+    
+    if (!rootNavController || ![rootNavController isKindOfClass:[UIViewController class]]) {
+        return nil;
+    }
+    
+    if ([rootNavController isKindOfClass:[UINavigationController class]]) {
+        UINavigationController *navController = rootNavController;
+        while ([navController isKindOfClass:[UINavigationController class]]) {
+            UIViewController *topViewController = [navController topViewController];
+            if ([topViewController isKindOfClass:[UINavigationController class]]) { //顶层是个导航控制器，继续循环
+                navController = (UINavigationController *) topViewController;
+            } else {
+                //是否有弹出presentViewControllr;
+                UIViewController *presentedViewController = topViewController.presentedViewController;
+                while (presentedViewController) {
+                    topViewController = presentedViewController;
+                    if ([topViewController isKindOfClass:[UINavigationController class]]) {
+                        break;
+                    } else {
+                        presentedViewController = topViewController.presentedViewController;
+                    }
+                }
+                navController = (UINavigationController *) topViewController;
+            }
+        }
+        return (UIViewController *) navController;
+    }else{
+        if (rootNavController.presentedViewController) {
+            return rootNavController.presentedViewController;
+        }
+        return rootNavController;
+    }
+}
+
 @end
